19b56bebd5dba0f7e16493509ca2afb282e2ebbd
Nikolay0951 <egnicholas333@gmail.com>
2019-10-02 10:26:37 +0300

    Вынес максимальный КБ в константу

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const regularPurchases = 3000;
 const increasedCashback = 5000;
 const specialOffers = 7000;
+const maxCashback = 3000;
 
 const primaryCashback = (regularPurchases * 0.01) + (increasedCashback * 0.05) + (specialOffers * 0.3);
-const totalCashback = primaryCashback > 3000 ? 3000 : primaryCashback;
+const totalCashback = primaryCashback > maxCashback ? maxCashback : primaryCashback;
 
 console.log("Начислено бонусов: " +totalCashback);
