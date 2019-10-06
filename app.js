7986b6986ceb32f4b91ae500bdfa892e2621a9ad
Nikolay0951 <egnicholas333@gmail.com>
2019-10-06 10:01:31 +0300

    Вынес проценты в константы

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const regularPurchases = 3000;
 const increasedCashback = 5000;
 const specialOffers = 7000;
+
+const regularPersent = 0.01;
+const increasedPercent = 0.05;
+const specialPercent = 0.3;
+
 const maxCashback = 3000;
 
-const primaryCashback = (regularPurchases * 0.01) + (increasedCashback * 0.05) + (specialOffers * 0.3);
+const primaryCashback = (regularPurchases * regularPersent) + (increasedCashback * increasedPercent) + (specialOffers * specialPercent);
 const totalCashback = primaryCashback > maxCashback ? maxCashback : primaryCashback;
 
-console.log("Начислено бонусов: " +totalCashback);
+console.log("Начислено бонусов: " + totalCashback);
